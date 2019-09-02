edba2836de4e61c14cd71afac90bf0e48ff13988
Liam Williams <l.s.w5559@gmail.com>
2019-09-02 13:47:58 +0100

    1.2 Beta 3 - multi deletion delay

--- XF/Entity/User.php
+++ XF/Entity/User.php
@@ -18,6 +18,18 @@ class User extends XFCP_User
 			return false;
 		}
 
+		if (\XF::options()->liamw_accountdelete_repeat_delay)
+		{
+			$recentDeletionInitiation = $this->getRelationFinder('AccountDeletionLogs')
+				->order('initiation_date', 'desc')->pluckFrom('initiation_date')->fetch(1)->first();
+
+			if ($recentDeletionInitiation && $recentDeletionInitiation > \XF::$time - (\XF::options()->liamw_accountdelete_repeat_delay * 24 * 60 * 60))
+			{
+				$error = \XF::phraseDeferred('liamw_accountdelete_you_cannot_delete_your_account_as_you_have_recently_cancelled_pending');
+				return false;
+			}
+		}
+
 		$criteria = $this->app()->criteria('XF:User', \XF::options()->liamw_accountdelete_user_criteria);
 		$criteria->setMatchOnEmpty(true);
 
