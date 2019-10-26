dbd23481a66ad0272256501a1014cad3d94f8e59
Liam Williams <l.s.w5559@gmail.com>
2019-10-26 11:24:24 +0100

    Beta 7 - Fix option format

--- Option/AccountDeleteMethod.php
+++ Option/AccountDeleteMethod.php
@@ -2,6 +2,7 @@
 
 namespace LiamW\AccountDelete\Option;
 
+use XF\Entity\Option;
 use XF\Option\AbstractOption;
 
 class AccountDeleteMethod extends AbstractOption
@@ -16,5 +17,35 @@ class AccountDeleteMethod extends AbstractOption
 		return self::getTemplate('admin:liamw_accountdelete_option_template_deletion_method', $option, $htmlParams, [
 			'userGroups' => $userGroupChoices
 		]);
+	}
+
+	public static function verifyOption(&$optionValue, Option $option, $optionId)
+	{
+		$disableOptions = [
+			'remove_email' => false,
+			'ban_email' => false,
+			'remove_password' => false,
+			'disabled_group_id' => 0
+		];
+
+		if (empty($optionValue['disable_options']))
+		{
+			$optionValue['disable_options'] = $disableOptions;
+		}
+
+		$optionValue['disable_options'] = array_replace($disableOptions, $optionValue['disable_options']);
+
+		$deleteOptions = [
+			'ban_email' => false
+		];
+
+		if (empty($optionValue['delete_options']))
+		{
+			$optionValue['delete_options'] = $deleteOptions;
+		}
+
+		$optionValue['delete_options'] = array_replace($deleteOptions, $optionValue['delete_options']);
+
+		return true;
 	}
 }
